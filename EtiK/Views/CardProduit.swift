40766dac9e62e6e27d9468a0437ba3037f33261f
Emilien <emiliengir@gmail.com>
2023-10-19 16:32:32 +0200

    Card produit

---
+++ EtiK/Views/CardProduit.swift
@@ -0,0 +1,18 @@
+//
+//  CardProduit.swift
+//  EtiK
+//
+//  Created by Émilien Giraud on 19/10/2023.
+//
+
+import SwiftUI
+
+struct CardProduit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardProduit()
+}
---
+++ EtiK/Views/PiluleStatut.swift
@@ -0,0 +1,18 @@
+//
+//  PiluleStatut.swift
+//  EtiK
+//
+//  Created by Émilien Giraud on 19/10/2023.
+//
+
+import SwiftUI
+
+struct PiluleStatut: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PiluleStatut()
+}
