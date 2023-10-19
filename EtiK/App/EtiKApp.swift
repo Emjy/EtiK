4c935bc510725ff23a8b68026aabdd2f7936560f
Emilien <emiliengir@gmail.com>
2023-10-19 14:14:57 +0200

    Initial Commit

---
+++ EtiK/App/EtiKApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtiKApp.swift
+//  EtiK
+//
+//  Created by Émilien Giraud on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct EtiKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LandingView()
+        }
+    }
+}
