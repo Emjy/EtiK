ca0f8b6d9a76eb7b31d23b3064d00ca07df42dce
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2023-10-20 10:45:32 +0200

    added files

---
+++ EtiK/Views/CreatorDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CreatorDetailView.swift
+//  EtiK
+//
+//  Created by Sylvain Leguay on 20/10/2023.
+//
+
+import SwiftUI
+
+struct CreatorDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatorDetailView()
+}
---
+++ EtiK/Views/FollowedCreatorView.swift
@@ -0,0 +1,18 @@
+//
+//  FollowedCreatorView.swift
+//  EtiK
+//
+//  Created by Sylvain Leguay on 20/10/2023.
+//
+
+import SwiftUI
+
+struct FollowedCreatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FollowedCreatorView()
+}
