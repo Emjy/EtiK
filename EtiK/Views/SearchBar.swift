3636235a3bfd7ffd6e20c7ce21834a8551c50819
Rui Da Silva Afonso <ruidasilvaafonso@gmail.com>
2023-10-20 10:58:40 +0200

    ajout searchBar

---
+++ EtiK/Views/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  EtiK
+//
+//  Created by apprenant48 on 20/10/2023.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
